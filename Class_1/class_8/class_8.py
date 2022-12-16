b0f947822b25168cdae7ca69a23bdc928aabec28
Hanamichi2002 <samyogacharya2058@gmail.com>
2022-12-16 07:51:35 +0545

    git

---
+++ Class_1/class_8/class_8.py
@@ -0,0 +1,12 @@
+# More about Dictionary 
+
+dict = {'a':'home','b':'school','C':'college'}
+dict["new"] = 3
+print (dict)
+net = dict.items()
+print(net)
+print(dict['a']=='home')
+print(dict.pop('a'))
+print(len(dict))
+print(dict.popitem())
+
