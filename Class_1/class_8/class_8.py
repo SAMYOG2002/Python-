edb8353c27527f8f280f9d4f14179677aa9980ad
Hanamichi2002 <samyogacharya2058@gmail.com>
2022-12-16 08:28:35 +0545

    nayea

--- Class_1/class_8/class_8.py
+++ Class_1/class_8/class_8.py
@@ -9,4 +9,28 @@ print(dict['a']=='home')
 print(dict.pop('a'))
 print(len(dict))
 print(dict.popitem())
+print(dict.update({'d': None}))
+print(dict.get('b'))
+dict["new_dict"] = {1: 'one', 2:'two'}
+print(dict)
+
+## Nested Dictionaries
+
+sagarmatha = {
+    "Department":{
+        "Computer Engineering":{
+            "HOD":"Bharat Bhatta", 
+            "NO of teachers":20
+        },
+        "Civil Engineering":{
+            "HOD":"Sudip Lamsal",
+            "NO of teachers":20
+        },
+        "Exam Department":{
+            "HOD":"Chaturbhuj Nepali",
+            "NO of teachers":10
+        }
+    }    
+}
+print(sagarmatha)
 
